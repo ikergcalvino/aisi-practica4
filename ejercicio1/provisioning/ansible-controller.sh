36a9ad3b040ffa095ffe7fe63682db6a619375a1
Roberto R. Expósito <rreye@udc.es>
2021-04-21 17:39:14 +0200

    Add sanity checks for ssh keys

--- ejercicio1/provisioning/ansible-controller.sh
+++ ejercicio1/provisioning/ansible-controller.sh
@@ -9,5 +9,16 @@ cp /vagrant/ansible.inventory /etc/ansible/hosts
 # Create ssh keys
 USER_DIR=/home/vagrant/.ssh
 echo -e 'y\n' | sudo -u vagrant ssh-keygen -t rsa -f $USER_DIR/id_rsa -q -N ''
+
+if [ ! -f $USER_DIR/id_rsa.pub ]; then
+	echo "SSH public key could not be created"
+	exit -1
+fi
+
 chown vagrant:vagrant $USER_DIR/id_rsa*
-cp $USER_DIR/id_rsa.pub /vagrant >& /dev/null
+cp $USER_DIR/id_rsa.pub /vagrant
+
+if [ ! -f /vagrant/id_rsa.pub ]; then
+	echo "SSH public key could not be copied"
+	exit -1
+fi
